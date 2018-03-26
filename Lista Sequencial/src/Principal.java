09a3187a03698379fb0afff6a8abc0bf953d88b1
henriquesan14 <33522361+henriquesan14@users.noreply.github.com>
2018-03-26 13:17:42 -0300

    Update Principal.java

--- Lista Sequencial/src/Principal.java
+++ Lista Sequencial/src/Principal.java
@@ -14,7 +14,7 @@ public class Principal {
 	public static void menu(){
 			int opc;
 			Scanner entrada=new Scanner(System.in);
-			System.out.println("Qual lista você deseja implementar? ");
+			System.out.println("Qual lista vocÃª deseja implementar? ");
 			System.out.println("1-Lista Sequencial ");
 			System.out.println("2-Lista Encadeada ");
 			System.out.println("3-Sair ");
@@ -23,7 +23,7 @@ public class Principal {
 			case 1: listaSeq();break;
 			case 2: listaEnc();break;
 			case 3: System.out.println("Encerrado!");break;
-			default:System.out.println("Opção Inválida!");menu();
+			default:System.out.println("OpÃ§Ã£o InvÃ¡lida!");menu();
 			}
 		
 	}
@@ -37,7 +37,7 @@ public class Principal {
 		if (opcao.equalsIgnoreCase("sim")){
 			Lista listasequencial=new Lista();
 			do{
-			System.out.println("O que você deseja fazer? ");
+			System.out.println("O que vocÃª deseja fazer? ");
 			System.out.println("1-Inserir elemento");
 			System.out.println("2-Remover elemento ");
 			System.out.println("3-Exibir a lista ");
@@ -49,23 +49,26 @@ public class Principal {
 				System.out.println("Qual nome do contato ?");
 				nome=entrada.next();
 				c1.setNome(nome);
-				System.out.println("Qual número do contato ?");
+				System.out.println("Qual nÃºmero do contato ?");
 				tel=entrada.next();
 				c1.setTelefone(tel);
-				System.out.println("Qual posição vc quer inserir ?");
+				System.out.println("Qual posiÃ§Ã£o vc quer inserir ?");
 				pos=entrada.nextInt();
-				listasequencial.inserir(pos, c1);			
+				listasequencial.inserir(pos, c1);
+				System.out.println("Elemento inserido");
 				break;
+				
 			case 2: 
-				System.out.println("Qual elemento você quer remover?");
+				System.out.println("Qual elemento vocÃª quer remover?");
 				posicao=entrada.nextInt();
 				listasequencial.remover(posicao);
+				System.out.println("Elemento removido");
 				break;
 			case 3:
 				listasequencial.exibirLista();
 				break;
 			case 4: menu();break;
-			default:System.out.println("Opção inválida");
+			default:System.out.println("OpÃ§Ã£o invÃ¡lida");
 			}
 			}while(op!=4);
 		}else{menu();}
@@ -81,7 +84,7 @@ public class Principal {
 		if (opcao.equalsIgnoreCase("sim")){
 			Lista2 listaencadeada=new Lista2();
 			do{
-			System.out.println("O que você deseja fazer? ");
+			System.out.println("O que vocÃª deseja fazer? ");
 			System.out.println("1-Inserir elemento");
 			System.out.println("2-Remover elemento ");
 			System.out.println("3-Exibir a lista ");
@@ -97,11 +100,12 @@ public class Principal {
 			idade=entrada.nextInt();
 			a1.idade=idade;
 			listaencadeada.inserirNoInicio(a1);
+			System.out.println("Elemento inserido");
 			break;
-			case 2: listaencadeada.removeAluno(a1);break;
+			case 2: listaencadeada.removeAluno(a1);System.out.println("Elemento removido");break;
 			case 3:listaencadeada.exibirLista();break;
 			case 4:menu();break;
-			default:System.out.println("Opção inválida");
+			default:System.out.println("OpÃ§Ã£o invÃ¡lida");
 			}
 			}while(op!=4);
 		}else{menu();}
